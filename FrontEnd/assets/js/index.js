fe9e6a85851e6031d4e30f06b78550713a0539c5
Kerelos Mikhail <Kerelos.Adel@gmail.com>
2025-02-14 03:03:16 -0500

    Adding index.js

---
+++ FrontEnd/assets/js/index.js
@@ -0,0 +1,15 @@
+// Using fetch to grap the work
+
+// create a funtion that generate a single work and attach it to 	<div class="gallery"> in line 40 index.html
+
+const work = {
+  id: 1,
+  title: "Tahina lampshade",
+  imageUrl: "http://localhost:5678/images/abajour-tahina1651286843956.png",
+  categoryId: 1,
+  userId: 1,
+  category: {
+    id: 1,
+    name: "Objects",
+  },
+};
